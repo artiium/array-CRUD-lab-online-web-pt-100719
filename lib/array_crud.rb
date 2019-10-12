3807d4bdfabff51853d240f6e74ccfdc16d7e603
Artiium Arbyummi <artiium21@gmail.com>
2019-10-12 04:00:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  family = ["Artiium", "Aqueelah", "Zaheer"]
+  family = ["Artiium", "Aqueelah", "Zaheer", "God"]
 end
 
 def add_element_to_end_of_array(array, element)
